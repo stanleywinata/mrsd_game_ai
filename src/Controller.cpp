fce1d820776ed170710e9f1696b23b403b39a4d1
stanleywinata <stanleyw9709@gmail.com>
2019-02-16 15:22:21 -0500

    wokring

--- src/Controller.cpp
+++ src/Controller.cpp
@@ -2,11 +2,21 @@
 #include <cmath>
 #include <iostream>
 #include <algorithm>
+#include <typeinfo>
 namespace mrsd
 {
 	void Controller::control(const mrsd::Game& g, float t)
 	{
-        pickSafeSpot(g);
+		std::vector<Player*> players = g.getPlayers();
+		if(!players.empty())
+        {
+            int des_pos = pickSafeSpot(g);
+            if(players[0]->x < des_pos)
+			    players[0]->x = players[0]->x+g.playerSpeed;
+
+            else if(players[0]->x > des_pos)
+			    players[0]->x = players[0]->x-g.playerSpeed;
+        }
 	}
 
 	void Controller::createPlayer(Game& g)
@@ -27,9 +37,11 @@ namespace mrsd
 	Prediction Controller::trackProjectile(const Projectile& p, const Game& g)
 	{
         Prediction pred;
-        double tf = (-p.vy+sqrt(pow(p.vy,2)+4*g.getGravity()/2*p.y))/(g.getGravity());
-        pred.t = g.getGameTime() + tf; // For if ask for game time, not time until explosion 
-        pred.x = p.x + p.vx*tf;
+        pred.t = (-p.vy+sqrt(pow(p.vy,2)+4*g.getGravity()/2*p.y))/(g.getGravity());
+        pred.x = p.x + p.vx*pred.t;
+        if(pred.x < 0 ){
+            pred.x = 0;
+        }
 		return pred;
 	}
 
@@ -39,22 +51,37 @@ namespace mrsd
         std::list<Projectile> projectiles = g.getProjectiles();
         for(std::list<Projectile>::iterator it = projectiles.begin(); it != projectiles.end(); ++it)
 		{
-            int idx = std::distance(projectiles.begin(), it);
             Prediction pred = trackProjectile(*it,g);
-            if(pred.x == 0 && pred.t==g.getGameTime()){
-                safespot[idx] = 0;
-                safespot[idx+g.explosionSize] = 0;
-                safespot[idx-g.explosionSize] = 0;
+            if(pred.t<1){
+                for(int i = std::floor(pred.x-g.explosionSize);i<=std::ceil(pred.x+g.explosionSize);i++)
+                {
+                    safespot[i] = 0;                
+                }
             }
 		}
+        // for(std::vector<int>::const_iterator it = safespot.begin();it != safespot.end();++it)
+        //     std::cout<<*it<<' ';
+        // std::cout<<"\n"<<"\n";
         return safespot;
 	}
 
 	int Controller::pickSafeSpot(const Game& g)
 	{
+       int dist_min = 1000000;
+       int pos;
 	   std::vector<int>  safespot = determineSafeSpots(g);
-       std::vector<int>::iterator it = std::find(safespot.begin(),safespot.end(),1);
-       auto pos = std::distance(safespot.begin(),it);
-       std::cout<<pos;
+	   std::vector<Player*> players = g.getPlayers();
+       // int index = players[0]->x;
+       for(std::vector<int>::iterator it = safespot.begin();it != safespot.end();++it){
+           if(*it==1){
+               auto index = std::distance(safespot.begin(),it);
+               int dist = std::abs(index-players[0]->x);
+               if(dist<dist_min){
+                   dist_min = dist;
+                   pos = index;
+               }
+           }
+       }
+       return pos;
 	}
 }
